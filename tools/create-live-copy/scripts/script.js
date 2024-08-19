3bf975805677297ad5e7ee9f1e654b907ab5df20
dianaapredaa <dianaapredaa@gmail.com>
2024-08-19 12:40:32 +0300

    create live copy

---
+++ tools/create-live-copy/scripts/script.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line func-names
+document.getElementById('liveCopyForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevents the default form submission
+
+  fetch('http://localhost:8080/api/submitForm', {
+    method: 'POST',
+    body: new FormData(this),
+  }).then((response) => {
+    if (response.ok) {
+      this.reset(); // Resets the form
+    }
+    // eslint-disable-next-line no-console
+  }).catch((error) => console.error('Error:', error));
+
+  // Form submitted successfully
+  // eslint-disable-next-line no-alert
+  alert('Form submitted successfully!');
+  this.reset(); // Reset the form
+});
