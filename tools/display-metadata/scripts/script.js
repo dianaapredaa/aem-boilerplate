b0ec7f97ae3e0955cdf187203aa3be6d18d73d13
Diana Preda <dpreda@adobe.com>
2024-08-20 15:41:54 +0300

    display metadata

---
+++ tools/display-metadata/scripts/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('metadata-form');
+  const metadataDisplay = document.getElementById('metadata-display');
+  const errorMessage = document.getElementById('errorMessage');
+
+  if (form) {
+    // eslint-disable-next-line func-names
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); // Prevents the default form submission
+
+      fetch('http://localhost:8080/api/metadata', {
+        method: 'POST',
+        body: new FormData(this),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json(); // Parse the JSON from the response
+          }
+          throw new Error('Network response was not ok.');
+        }).then((data) => {
+          // Check if 'data' has the expected properties
+          document.getElementById('name').textContent = data.name || 'N/A';
+          document.getElementById('id').textContent = data.id || 'N/A';
+          document.getElementById('createdBy').textContent = data.createdBy || 'N/A';
+          document.getElementById('createdDateTime').textContent = data.createdDateTime || 'N/A';
+          document.getElementById('lastModifiedBy').textContent = data.lastModifiedBy || 'N/A';
+          document.getElementById('lastModifiedDateTime').textContent = data.lastModifiedDateTime || 'N/A';
+
+          form.style.display = 'none'; // Hide the form
+          metadataDisplay.style.display = 'block'; // Show the metadata details
+          errorMessage.textContent = ''; // Clear any previous error message
+          // eslint-disable-next-line no-console
+        }).catch((error) => console.error('Error:', error));
+    });
+  }
+});
