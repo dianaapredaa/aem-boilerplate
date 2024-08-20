b63f9c45ed9638452ac3280db96e3470153e68ce
dianaapredaa <dianaapredaa@gmail.com>
2024-08-20 09:44:14 +0300

    create live copy

--- tools/create-live-copy/scripts/script.js
+++ tools/create-live-copy/scripts/script.js
@@ -1,26 +1,4 @@
-// eslint-disable-next-line max-len
-// logic to autocomplete the blueprint path filed with the path to the file we have activated the form from
-
-// Get the current file path
-const currentPath = window.location.pathname;
-
-// Get the blueprint path field
-const blueprintPath = document.getElementById('blueprintPath');
-
-// Set the blueprint path field value to the current path
-if (blueprintPath) {
-  blueprintPath.value = currentPath;
-
-  // Add a change event listener to the blueprint path field
-
-  blueprintPath.addEventListener('change', () => {
-    // Set the blueprint path field value to the current path
-    blueprintPath.value = currentPath;
-  });
-}
-
-// eslint-disable-next-line func-names
-document.getElementById('liveCopyForm').addEventListener('submit', function (event) {
+document.getElementById('liveCopyForm').addEventListener('submit', (event) => {
   event.preventDefault(); // Prevents the default form submission
 
   fetch('http://localhost:8080/api/submitForm', {
@@ -29,9 +7,15 @@ document.getElementById('liveCopyForm').addEventListener('submit', function (eve
   }).then((response) => {
     if (response.ok) {
       this.reset(); // Resets the form
+      const successMessage = document.getElementById('successMessage');
+      if (successMessage) {
+        successMessage.textContent = 'Form submitted successfully!';
+      }
     }
-    // eslint-disable-next-line no-console
-  }).catch((error) => console.error('Error:', error));
-
-  this.reset(); // Reset the form
+  }).catch(() => {
+    const errorMessage = document.getElementById('errorMessage');
+    if (errorMessage) {
+      errorMessage.textContent = 'An error occurred while submitting the form. Please try again.';
+    }
+  });
 });
