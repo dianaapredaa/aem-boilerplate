1932410844ec2e5ecdee0fedcd623572ea40a384
Diana Preda <dpreda@adobe.com>
2024-08-22 17:07:49 +0300

    display metadata

--- tools/live-copy/scripts/script.js
+++ tools/live-copy/scripts/script.js
@@ -1,8 +1,30 @@
 // eslint-disable-next-line func-names
-document.getElementById('liveCopyForm').addEventListener('submit', function (event) {
+document.getElementById('create-live-copy').addEventListener('submit', function (event) {
   event.preventDefault(); // Prevents the default form submission
 
-  fetch('http://localhost:8080/api/submit-form', {
+  fetch('http://localhost:8080/api/create-live-copy', {
+    method: 'POST',
+    body: new FormData(this),
+  }).then((response) => {
+    if (response.ok) {
+      this.reset(); // Resets the form
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('Form submission failed!');
+    }
+    // eslint-disable-next-line no-console
+  }).catch((error) => console.error('Error:', error));
+
+  // eslint-disable-next-line no-alert
+  alert('Form submitted successfully!');
+  this.reset(); // Reset the form
+});
+
+// eslint-disable-next-line func-names
+document.getElementById('delete-live-copy').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevents the default form submission
+
+  fetch('http://localhost:8080/api/delete-live-copy', {
     method: 'POST',
     body: new FormData(this),
   }).then((response) => {
