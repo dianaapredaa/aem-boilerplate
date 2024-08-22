b013e94fa20620fc4db6d8d103ef36f93b1bac08
Diana Preda <dpreda@adobe.com>
2024-08-22 18:27:03 +0300

    display metadata

---
+++ tools/rollout/scripts/script.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line func-names
+document.getElementById('rollout').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevents the default form submission
+
+  fetch('http://localhost:8080/api/rollout', {
+    body: new FormData(this),
+  }).then((response) => {
+    if (response.ok) {
+      this.reset(); // Resets the form
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('Form submission failed!');
+    }
+    // eslint-disable-next-line no-console
+  }).catch((error) => console.error('Error:', error));
+
+  // eslint-disable-next-line no-alert
+  alert('Form submitted successfully!');
+  this.reset(); // Reset the form
+});
