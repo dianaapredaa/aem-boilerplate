6d35d248c75919dbe14525bd0b35b75589fae21e
Diana Preda <dpreda@adobe.com>
2024-08-26 13:36:11 +0300

    rollout

---
+++ tools/enable-inheritance/scripts/script.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line func-names
+document.getElementById('create-live-copy').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevents the default form submission
+
+  fetch('http://localhost:8080/api/enable-inheritance', {
+    method: 'POST',
+    body: new FormData(this),
+  }).then((response) => {
+    if (response.ok) {
+      this.reset(); // Resets the form
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('Form submission failed!');
+    }
+    // eslint-disable-next-line no-console
+  }).catch((error) => console.error('Error:', error));
+
+  // eslint-disable-next-line no-alert
+  alert('Enabling Inheritance!');
+  this.reset(); // Reset the form
+});
