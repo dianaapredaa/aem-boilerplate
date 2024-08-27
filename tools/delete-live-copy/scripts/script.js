85715e1182fd031adb2bd301be42c4c00092f149
Diana Preda <dpreda@adobe.com>
2024-08-27 15:14:47 +0300

    document path

--- tools/delete-live-copy/scripts/script.js
+++ tools/delete-live-copy/scripts/script.js
@@ -1,3 +1,13 @@
+// check if any file is selected in the Sharepoint when the form is submitted
+document.getElementById('delete-live-copy').addEventListener('submit', (event) => {
+  const file = document.getElementById('file').files[0];
+  if (!file) {
+    // eslint-disable-next-line no-alert
+    alert('Please select a file!');
+    event.preventDefault(); // Prevents the form submission
+  }
+});
+
 // eslint-disable-next-line func-names
 document.getElementById('delete-live-copy').addEventListener('submit', function (event) {
   event.preventDefault(); // Prevents the default form submission
