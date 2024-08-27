599beedec241349aad99dda0ccc57e0883ccd677
Diana Preda <dpreda@adobe.com>
2024-08-27 15:09:54 +0300

    document path

--- tools/create-live-copy/scripts/script.js
+++ tools/create-live-copy/scripts/script.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line func-names
 document.getElementById('file-input').addEventListener('change', function () {
-  const file = this.files[0];
-  if (file) {
-    // eslint-disable-next-line no-alert
-    alert(`File selected: ${file.name}`);
+  document.getElementById('create-live-copy');
+  if (this.files.length === 0) {
+    console.log('No file selected.');
+  } else {
+    console.log('File selected:', this.files[0].name);
   }
 });
-
 // eslint-disable-next-line func-names
 document.getElementById('create-live-copy').addEventListener('submit', function (event) {
   event.preventDefault(); // Prevents the default form submission
