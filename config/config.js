98d71a4c9c14eb812124c8819fcbab70476ce643
Diana Preda <dpreda@adobe.com>
2024-11-18 13:55:08 +0200

    deploy

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 // config/config.js
 // eslint-disable-next-line import/prefer-default-export
-export const apiUrl = 'https://aem-sites-reverie-msm-launches-eds-deploy-ethos12-416093.stage.cloud.adobe.io/sharepoint/create-live-copy';
+export const apiUrl = 'https://aem-sites-reverie-msm-launches-eds-deploy-ethos12-416093.stage.cloud.adobe.io';
