5c42a778d3775b57ab0d7a028f76fae45ed023d3
Diana Preda <dpreda@adobe.com>
2024-11-19 14:01:09 +0200

    script

--- tools/create-live-copy/scripts/script.js
+++ tools/create-live-copy/scripts/script.js
@@ -6,6 +6,7 @@ document.getElementById('create-live-copy').addEventListener('submit', function 
 
   fetch(`${apiUrl}/sharepoint/create-live-copy`, {
     method: 'POST',
+    mode: 'no-cors',
     headers: {
       Accept: 'application/json',
     },
